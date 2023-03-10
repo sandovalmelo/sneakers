0f420311478dbb932a9d223ce964306e2ed6b62a
Sandoval Melo <juniormelo777@gmail.com>
2023-03-10 18:08:12 -0300

    add quantity control scripts

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@ const menuIcon = document.getElementById("menu-icon");
 const menuNav = document.getElementById("menu-nav");
 const menuIconImg = document.querySelector("#menu-icon img");
 
+const plusBtn = document.getElementById("add");
+const minusBtn = document.getElementById("minus");
+const productQuantity = document.getElementById("quantity");
+let quantity = 1;
+
 menuIcon.addEventListener("click", (event) => {
 	console.log(menuNav.classList.contains("active"));
 	if (!menuNav.classList.contains("active")) {
@@ -12,5 +17,17 @@ menuIcon.addEventListener("click", (event) => {
 		menuNav.classList.remove("active");
 		menuIconImg.setAttribute("src", "./images/icon-menu.svg");
 		document.body.style.overflow = "scroll";
+	}
+});
+
+plusBtn.addEventListener("click", (event) => {
+	quantity++;
+	productQuantity.innerText = quantity;
+});
+
+minusBtn.addEventListener("click", (event) => {
+	if (quantity > 0) {
+		quantity--;
+		productQuantity.innerText = quantity;
 	}
 });
