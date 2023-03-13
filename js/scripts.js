8f02881f7364760a25fffda1e042fc4ef2345799
Sandoval Melo <juniormelo777@gmail.com>
2023-03-13 17:15:43 -0300

    add cart details

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,9 @@ const minusBtn = document.getElementById("minus");
 const productQuantity = document.getElementById("quantity");
 let quantity = 1;
 
+const addToCartBtn = document.getElementById("add-to-cart");
+const cartItems = document.getElementById("cart-itens");
+
 menuIcon.addEventListener("click", (event) => {
 	console.log(menuNav.classList.contains("active"));
 	if (!menuNav.classList.contains("active")) {
@@ -29,5 +32,14 @@ minusBtn.addEventListener("click", (event) => {
 	if (quantity > 0) {
 		quantity--;
 		productQuantity.innerText = quantity;
+	}
+});
+
+addToCartBtn.addEventListener("click", (event) => {
+	cartItems.classList.add("active");
+	cartItems.innerText = quantity;
+
+	if (quantity === 0) {
+		cartItems.classList.remove("active");
 	}
 });
