4af9a90c675eb68a5c4617472591afa81a454c35
Sandoval Melo <juniormelo777@gmail.com>
2023-03-16 16:03:19 -0300

    add toggle cart details scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 const menuIcon = document.getElementById("menu-icon");
 const menuNav = document.getElementById("menu-nav");
+const navList = document.getElementById("nav-list");
 const menuIconImg = document.querySelector("#menu-icon img");
 
 const plusBtn = document.getElementById("add");
@@ -9,9 +10,18 @@ let quantity = 1;
 
 const addToCartBtn = document.getElementById("add-to-cart");
 const cartItems = document.getElementById("cart-itens");
+const cartContainer = document.getElementById("cart-container");
+const menuCart = document.getElementById("menu-cart");
+
+menuNav.addEventListener("click", (event) => {
+	if (!event.target.closest("#nav-list")) {
+		menuNav.classList.remove("active");
+		menuIconImg.setAttribute("src", "./images/icon-menu.svg");
+		document.body.style.overflow = "scroll";
+	}
+});
 
 menuIcon.addEventListener("click", (event) => {
-	console.log(menuNav.classList.contains("active"));
 	if (!menuNav.classList.contains("active")) {
 		menuNav.classList.add("active");
 		menuIconImg.setAttribute("src", "./images/icon-close.svg");
@@ -42,4 +52,8 @@ addToCartBtn.addEventListener("click", (event) => {
 	if (quantity === 0) {
 		cartItems.classList.remove("active");
 	}
+});
+
+menuCart.addEventListener("click", (event) => {
+	cartContainer.classList.toggle("active");
 });
