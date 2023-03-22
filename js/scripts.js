d67e2e942cb4c4027d564dfdb21252fbe01964a1
Sandoval Melo <juniormelo777@gmail.com>
2023-03-22 18:38:23 -0300

    add infinity slide scripts

--- js/scripts.js
+++ js/scripts.js
@@ -18,10 +18,53 @@ const deleteIcon = document.getElementById("delete-icon");
 const cartQuantity = document.getElementById("cart-quantity");
 const cartTotal = document.getElementById("cart-total");
 
-const sliderContainer = document.getElementById("slider-container");
-const nextBtn = document.getElementById("next-btn");
-const prevBtn = document.getElementById("prev-btn");
-const productThumbs = Array.from(document.querySelectorAll("#product-thumbs img"));
+// const sliderContainer = document.getElementById("slider-container");
+// const sliderItems = document.getElementById("slides");
+// const slides = document.getElementsByClassName("slide");
+// const slidesLength = slides.length;
+// const slideSize = slides[0].offsetWidth;
+// console.log(slides[0].offsetWidth);
+
+// const firstSlide = slides[0];
+// const lastSlide = slides[slidesLength - 1];
+// const cloneFirst = firstSlide.cloneNode(true);
+// const cloneLast = lastSlide.cloneNode(true);
+
+// let index = 0;
+// let allowShift = true;
+// let posInitial;
+// let posFinal;
+
+// sliderItems.appendChild(cloneFirst);
+// sliderItems.insertBefore(cloneLast, firstSlide);
+
+// const nextBtn = document.getElementById("next-btn");
+// const prevBtn = document.getElementById("prev-btn");
+// const productThumbs = Array.from(document.querySelectorAll("#product-thumbs img"));
+
+// function shiftSlide(direction) {
+// 	sliderItems.classList.add("shifting");
+
+// 	if (allowShift) {
+// 		posInitial = sliderItems.offsetLeft;
+
+// 		if (direction === 1) {
+// 			sliderItems.style.left = `${posInitial - slideSize}px`;
+// 			index++;
+// 		} else if (direction === -1) {
+// 			sliderItems.style.left = `${posInitial + slideSize}px`;
+// 			index--;
+// 		}
+// 	}
+// }
+
+// nextBtn.addEventListener("click", (event) => {
+// 	shiftSlide(1);
+// });
+
+// prevBtn.addEventListener("click", (event) => {
+// 	shiftSlide(-1);
+// });
 
 menuNav.addEventListener("click", (event) => {
 	if (!event.target.closest("#nav-list")) {
@@ -86,8 +129,10 @@ nextBtn.addEventListener("click", (event) => {
 	if (activeSlider < 4) {
 		const distance = sliderContainer.clientWidth;
 		sliderContainer.style.transform = `translateX(-${distance * activeSlider}px)`;
+		sliderContainer.style.transition = "transform ease 500ms";
 		activeSlider++;
 	}
+	console.log(activeSlider);
 });
 
 prevBtn.addEventListener("click", (event) => {
@@ -97,6 +142,7 @@ prevBtn.addEventListener("click", (event) => {
 		const total = distance * activeSlider;
 		sliderContainer.style.transform = `translateX(-${total - sliderContainer.clientWidth}px)`;
 	}
+	console.log(activeSlider);
 });
 
 productThumbs.forEach((thumb) => {
@@ -104,5 +150,6 @@ productThumbs.forEach((thumb) => {
 		const distance = sliderContainer.clientWidth;
 		const positionNum = parseInt(event.target.dataset.position - 1);
 		sliderContainer.style.transform = `translateX(-${distance * positionNum}px)`;
+		activeSlider = positionNum + 1;
 	});
 });
