cbe274974276b571ed281d4f5a0024f10a44e4cb
Sandoval Melo <juniormelo777@gmail.com>
2023-03-23 17:54:41 -0300

    add close overlay on click

--- js/overlay.js
+++ js/overlay.js
@@ -2,11 +2,17 @@ const overlay = document.getElementById("overlay");
 const closeOverlay = document.getElementById("close-overlay");
 
 closeOverlay.addEventListener("click", (event) => {
-	overlay.style.display = "none";
+	overlay.setAttribute("data-open", "false");
 });
 
 sliderItems.addEventListener("click", (event) => {
 	if (window.innerWidth > 899) {
-		overlay.style.display = "flex";
+		overlay.setAttribute("data-open", "true");
+	}
+});
+
+overlay.addEventListener("click", (event) => {
+	if (!event.target.closest(".overlay-container")) {
+		overlay.setAttribute("data-open", "false");
 	}
 });
