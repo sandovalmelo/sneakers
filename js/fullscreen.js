90a3fbbf7446e7190dbb2d81ba598f302c69ef02
Sandoval Melo <juniormelo777@gmail.com>
2023-03-25 17:32:55 -0300

    add slide function

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,6 +1,12 @@
 const fullscreen = document.getElementById("fullscreen");
 const closeFullscreen = document.getElementById("close-fullscreen");
 
+const fullscreenWrarpper = document.getElementById("fullscreen-wrarpper");
+const fullscreenSlides = document.getElementById("fullscreen-slides");
+const nextBtnFullscreen = document.getElementById("next-btn-fullscreen");
+const prevBtnFullscreen = document.getElementById("prev-btn-fullscreen");
+const fullscreenThumbs = document.getElementById("fullscreen-thumbs");
+
 closeFullscreen.addEventListener("click", (event) => {
 	fullscreen.setAttribute("data-open", "false");
 });
@@ -8,6 +14,14 @@ closeFullscreen.addEventListener("click", (event) => {
 sliderItems.addEventListener("click", (event) => {
 	if (window.innerWidth > 899) {
 		fullscreen.setAttribute("data-open", "true");
+		distance = fullscreenWrarpper.clientWidth;
+		slide(
+			fullscreenWrarpper,
+			fullscreenSlides,
+			prevBtnFullscreen,
+			nextBtnFullscreen,
+			fullscreenThumbs
+		);
 	}
 });
 
